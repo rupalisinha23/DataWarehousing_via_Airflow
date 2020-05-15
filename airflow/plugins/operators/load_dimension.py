cc5c3a039facfa93543f3b95f1ecead388f00239
Rupali Sinha <rupali.sinha@dfki.de>
2020-05-15 10:41:39 +0200

    Added mode parameter in load_dimension

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -11,6 +11,7 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id,
                  table,
                  sql_stmt,
+                 mode,
                  *args,
                  **kwargs):
 
@@ -22,6 +23,11 @@ class LoadDimensionOperator(BaseOperator):
 
     def execute(self, context):
         redshift = PostgresHook(self.redshift_conn_id)
+        
+        if self.mode == 'delete':
+            self.log.info(f'Deleting data from {self.table} dimension table.')
+            redshift.run(f'DELETE FROM {self.table};')
+            self.log.info("Delete is now complete.")
         
         self.log.info(f"Loading data into {self.table} table.")
         formatted_sql = f""" INSERT INTO {self.table} ({self.sql_stmt})"""
